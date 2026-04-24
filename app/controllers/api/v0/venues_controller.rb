0d12666218a3efdc13ab8a1fe0391c0c76df1987
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-04-24 12:45:24 +0500

    chore: using api error handling

--- app/controllers/api/v0/venues_controller.rb
+++ app/controllers/api/v0/venues_controller.rb
@@ -59,29 +59,5 @@ module Api::V0
 
       handle_operation_response(result)
     end
-
-    private
-
-    def handle_operation_response(result, success_status = :ok)
-      if result.success
-        render json: {
-          success: true,
-          data: result.value[:json]
-        }, status: success_status
-      else
-        handle_operation_failure(result)
-      end
-    end
-
-    def handle_operation_failure(result)
-      errors = result.errors
-
-      case errors
-      when :unauthorized
-        forbidden_response("You are not authorized to perform this action")
-      else
-        unprocessable_entity(errors)
-      end
-    end
   end
 end
